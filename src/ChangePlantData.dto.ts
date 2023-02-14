c78bb5b8253ffc9d648e816956680bebc1e6fa82
PfefferBotond <botond.pfeffer@gmail.com>
2023-02-14 11:54:26 +0100

    a

--- src/ChangePlantData.dto.ts
+++ src/ChangePlantData.dto.ts
@@ -7,4 +7,3 @@ export default class ChangePlantsData {
   @IsOptional()
   plantcolor: string;
 }
-
--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import UserData from './UserData.dto';
       host: 'localhost',
       username: 'root',
       password: '',
-      database: 'Novenyek',
+      database: 'novenyek',
       entities: [UserData],
       synchronize: true,
     }),
